5eaca110db1854cf11deffc4622af87cd295fca1
StefanyABL <73035293+StefanyABL@users.noreply.github.com>
2021-07-19 22:22:58 -0500

    Add files via upload

---
+++ Ejercicio.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Ejercicio.h
+ * Author: Usuario
+ *
+ * Created on 2 de septiembre de 2018, 09:23 PM
+ */
+
+#ifndef EJERCICIO_H
+#define EJERCICIO_H
+
+class Ejercicio {
+public:
+    Ejercicio();
+    Ejercicio(const Ejercicio& orig);
+    virtual ~Ejercicio();
+private:
+
+};
+
+#endif /* EJERCICIO_H */
+
